66aaebea387058baaebea3c30e04c15077b09296
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-10-21 19:33:15 +0200

    first commit

---
+++ datatypes/index.js
@@ -0,0 +1,6 @@
+//create function and 4 parameter for and devide for multiplication
+function avg1(house1, house2, house3, house4) {
+  multiplication = house1 * house2 * house3 * house4;
+  result = multiplication / 4;
+  console.log(result);
+}
