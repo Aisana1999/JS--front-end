4875155c304bb956bdc2c7c05f64f8c99f9fe41e
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-10-21 19:50:00 +0200

    printed the result of second code

--- datatypes/index.js
+++ datatypes/index.js
@@ -9,3 +9,10 @@ house2 = 20;
 house3 = 30;
 house4 = 40;
 avg1(house1, house2, house3, house4); //60000
+
+//create function and 4 parameter for and devide for multiplication
+function avg(house1, house2, house3, house4) {
+  addition = house1 + house2 + house3 + house4;
+  result1 = addition / 4;
+  console.log(result);
+}
