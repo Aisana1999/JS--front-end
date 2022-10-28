3ea7093d37f063cf74f07778327e4e8ca372f69b
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-10-28 23:31:07 +0200

    printed multiplication table for 5 and 8

--- loops/loop.js
+++ loops/loop.js
@@ -7,3 +7,14 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+//print multiplication table for any number
+for (var n = 1; n <= 100; n++) {
+  if (n % 5 == 0) {
+    console.log(n);
+  }
+}
+for (var n = 1; n <= 100; n++) {
+  if (n % 8 == 0) {
+    console.log(n);
+  }
+}
