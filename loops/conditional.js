cae2553e0dabc391a0c407383140de1d2b187af3
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-10-28 22:09:20 +0200

    added new condition

--- loops/conditional.js
+++ loops/conditional.js
@@ -4,10 +4,13 @@ let password = "rainbow123";
 let providedUsername = "john.smith";
 let providedPassword = "rainbow123";
 
+const isUserAlreadyLoggedIn = true;
 const result = username == providedUsername && password == providedPassword;
 
 if (result) {
   console.log("Access is Granted");
+} else if (isUserAlreadyLoggedIn) {
+  console.log("Access is Granted as User is already logged in.");
 } else {
   console.log("Incorrect Credentials");
 }
