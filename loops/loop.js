7c180c75dcc4b6f09084ae1702f864bbb90df241
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-10-28 23:47:47 +0200

    tried a different approach to print a table

--- loops/loop.js
+++ loops/loop.js
@@ -1,20 +1,22 @@
-for (let i = 1; i <= 100; i++) {
-  console.log(i);
-}
+//for (let i = 1; i <= 100; i++) {
+//console.log(i);
+//}
 //print all even numbers from 1 to 100
+//for (let i = 1; i <= 100; i++) {
+//if (i % 2 == 0) {
+//console.log(i);
+//}
+//}
+//print multiplication table for any number
+var num = 9;
 for (let i = 1; i <= 100; i++) {
-  if (i % 2 == 0) {
+  if (i % num == 0) {
     console.log(i);
   }
 }
-//print multiplication table for any number
-for (var n = 1; n <= 100; n++) {
-  if (n % 5 == 0) {
-    console.log(n);
-  }
-}
-for (var n = 1; n <= 100; n++) {
-  if (n % 8 == 0) {
-    console.log(n);
-  }
-}
+
+//for (var n = 1; n <= 100; n++) {
+//if (n % 8 == 0) {
+//console.log(n);
+//}
+//}
