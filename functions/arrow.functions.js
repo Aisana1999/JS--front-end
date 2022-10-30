810dd678c8bfa7582f14c4fe627a59337e1d9673
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-10-30 15:11:21 +0100

    printed triangle of stars

--- functions/arrow.functions.js
+++ functions/arrow.functions.js
@@ -15,5 +15,35 @@
 //};
 //primeNumber(100);
 
-const add = (a, b) => a + b;
-console.log(add(5, 4));
+//const add = (a, b) => a + b;
+//console.log(add(5, 4));
+
+// *
+// **
+// ***
+// ****
+// *****
+
+const printStars = (n) => {
+  // Loop is for traversing lines.
+  for (let i = 1; i <= n; i++) {
+    let stars = "";
+    // Inner loop.
+    // *****
+    // Creating stars on each line.
+    for (let j = 1; j <= i; j++) {
+      //  stars=stars+"*";
+      stars += "*";
+    }
+    console.log(stars);
+  }
+};
+
+printStars(5);
+
+// when n = 5
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
