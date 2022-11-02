6a13863c041c51a1c826304096e7c3ac56288107
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-02 20:41:55 +0100

    finding characters from location

---
+++ functions/strings/string.methods.js
@@ -0,0 +1,19 @@
+var location = "street24, NYC";
+var streetNumber = location[6] + location[7];
+console.log(streetNumber);
+console.log(location[12]);
+
+console.log(location.length);
+console.log(location.length - 1);
+
+console.log(location.indexof("2"));
+
+console.log(location.slice(10, 13));
+
+const replacedString = location.replace("NYC", "WA");
+console.log(replacedString);
+
+console.log(replacedString.toLowerCase());
+console.log(replacedString, toUpperCase());
+
+let username;
