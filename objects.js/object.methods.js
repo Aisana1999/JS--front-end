6ac39957b0df49c02f4f2e7d3156c649ec2cac48
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-04 22:07:02 +0100

    created objects

---
+++ objects.js/object.methods.js
@@ -0,0 +1,51 @@
+var student = {
+  id: 1,
+  name: "John Doe",
+  subject: ["CS", "Math", "psychology"],
+  address: {
+    buildingNo: 5,
+    streetNo: 25,
+    City: "NYC",
+    Country: "USA",
+  },
+};
+
+var tweet = {
+  id: 1,
+  content: "Hello people",
+  author: "Elon Musk",
+  timeoftweet: "2.Nov,2022",
+  likes: 200000,
+  comments: {
+    id: 2,
+    author: "Bill Gates",
+    content: "Hello to you too",
+    timestamp: "2 Nov,2022",
+  },
+};
+
+var message = {
+  id: 1,
+  author: "JD",
+  content: "Hello",
+  receiverID: 34,
+  isThisAGroupMessage: true,
+  group: {
+    name: Atlas,
+    admin: Tobias,
+  },
+};
+
+var post = {
+  id: 5,
+  author: "AT",
+  content: "news",
+  timeofpost: "3.Nov,2022",
+  likes: 1500,
+  comments: {
+    id: 3,
+    author: "Sasha",
+    content: "Good",
+    timestamp: "6 Nov,2022",
+  },
+};
