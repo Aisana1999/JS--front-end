8c21a0e7f41421e8b623a29ee97abf3f82527bfc
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-07 20:19:17 +0100

    created a file and inner function

---
+++ closure/closure.js
@@ -0,0 +1,14 @@
+function add() {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+  return fucntion;
+  addNext(c);
+  {
+    console.log(a + b + c);
+  }
+}
+const childFunction = add();
+
+console.log(childFunction);
+childFunction(30);
