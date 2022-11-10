bcb406f26fb1b1b32795d88d9fdcb20a885b02d9
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-10 22:09:49 +0100

    created a class

--- oops/tweet.js
+++ oops/tweet.js
@@ -1,1 +1,27 @@
-class tweet {}
+class tweet {
+  name;
+  id;
+  content: "Hello guys";
+  timeoftweet: "2 Nov,2022";
+  likes: 10455;
+  comments: [
+    {
+      name: Ronaldo,
+      id: 2,
+      content: "Nice picture",
+      timeofstamp: "2 Nov, 2022",
+    },
+    {
+      name: Messy,
+      id: 3,
+      content: "Good",
+      timeofstamp: "2 Nov, 2022",
+    },
+    {
+      name: Polo,
+      id: 4,
+      content: "Cool",
+      timeofstamp: "2 Nov, 2022",
+    }
+  ];
+}
