1ba941a0f13e265368d887ea53630778819a2b59
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-23 22:08:11 +0100

    created classes

---
+++ oops/assignments/seller.js
@@ -0,0 +1,22 @@
+class seller {
+  name = "";
+  id;
+  address;
+  mobile;
+  email;
+}
+addItemToCard(item);
+{
+  console.log("Added item to card");
+}
+placeorder();
+{
+  console.log("Order placed");
+}
+seeProduct();
+{
+  console.log("Browsing through products");
+}
+
+var customer1 = new customer();
+var customer2 = new customer();
