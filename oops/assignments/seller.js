b4b113d9c9ab6e2c1627447ff9be340624cf37fb
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-25 20:46:25 +0100

    created a constructor

--- oops/assignments/seller.js
+++ oops/assignments/seller.js
@@ -1,10 +1,20 @@
 class seller {
-  name = "aida";
-  id = 13984;
+  //properties
+  name = "";
+  id = "";
   address;
   mobile;
   email;
 }
+constructor(_name, _id, _address, _mobile, _email), //parameter
+  {
+    this: (name = _name),
+    this: (id = _id),
+    this: (address = _address),
+    this: (mobilePhone = _mobile),
+    this: (email = _email),
+  };
+
 sendEmail(email);
 {
   console.log("Email was sent");
