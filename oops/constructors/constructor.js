8984ceb5d44a3914117a9ec0d70b5137e765744a
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-11-26 22:33:31 +0100

    created constructor

--- oops/constructors/constructor.js
+++ oops/constructors/constructor.js
@@ -1,1 +1,40 @@
-constructor(), {};
+class Seller {
+  //properties
+  name = "";
+  id = "";
+  address;
+  mobile;
+  email = "";
+}
+constructor(_name, _id, _address, _mobile, _email), //parameter
+  {
+    this: (name = _name),
+    this: (id = _id),
+    this: (address = _address),
+    this: (mobilePhone = _mobile),
+    this: (email = _email),
+  };
+
+sendEmail(email);
+{
+  console.log("Email was sent");
+}
+placeorder();
+{
+  console.log("Order placed");
+}
+seeProduct();
+{
+  console.log("Browsing through products");
+}
+
+var seller1 = new seller(
+  "Ronaldo",
+  2,
+  "Portugal",
+  6783542122,
+  "seller@gmail.com"
+);
+console.log(seller1.email);
+var seller2 = new seller("Messy", 3, "Italy", 3457291843, "seller2@gmail.com");
+console.log(seller2.email);
