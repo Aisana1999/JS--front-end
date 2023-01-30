35319fa766f0c0ec5a297ea717623ce1c4bc73f9
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-01-30 15:42:11 +0100

    created a child property

--- oops/inheritance.js
+++ oops/inheritance.js
@@ -1,5 +1,7 @@
 class Gold Customer extends Customer{
     gold Service;
-    constructor (_goldService,_name,_id,_address,_phone,_email)
-    super(_name,_id,_adress,_phone,_email)
+    constructor (_goldService,_name,_id,_address,_phone,_email){
+     super(_name,_id,_adress,_phone,_email)
+    this.goldService=_goldService;   
+    }
 }
